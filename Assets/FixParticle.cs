3ef0b84feba789e7c4de75212dc990c8150953a0
Andrey <Andrey@AZAZAL>
2018-06-13 00:32:12 -0400

    Add project files.

---
+++ Assets/FixParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FixParticle : MonoBehaviour {
+    ParticleSystem _particle;
+
+	// Use this for initialization
+	void Start () {
+        _particle = GetComponent<ParticleSystem>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var rot = _particle.main;
+   rot.startRotationY = transform.localToWorldMatrix.rotation.eulerAngles.y * Mathf.Deg2Rad; 
+       //sr.constant  
+
+
+
+    }
+}
---
+++ Assets/Scripts/InputDriver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDriver : MonoBehaviour {
+    private Ship _ship;
+    // Use this for initialization
+    void Start()
+    {
+        _ship = GetComponent<Ship>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        _ship.StrafeHorizontal(Input.GetAxis("Horizontal"));        
+        _ship.StafeVertical(Input.GetAxis("Vertical"));
+
+        if (Input.GetAxis("LookH") != 0 || Input.GetAxis("LookV") != 0)
+        {
+            var newRotation = new Vector2(Input.GetAxis("LookH"),  Input.GetAxis("LookV"));
+            _ship.RotateTo(newRotation);
+        }
+
+    }
+}
